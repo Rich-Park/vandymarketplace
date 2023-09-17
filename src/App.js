2572a1e0d28cb8b66e4914c42089b30a12ad6e69
Rich Park <rich.park@vanderbilt.edu>
2023-09-17 13:12:52 -0500

    rich-homepage: added starting ui for homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import { ChakraProvider, Button } from '@chakra-ui/react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import SellItemForm from './SellItemForm';
+import { ChakraProvider, Button, extendTheme } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SellItemForm from "./components/SellItemForm";
+import HomePage from "./components/HomePage";
+
+const colors = {
+  brand: {
+    100: "#000000", // main black
+    200: "#A8996E", // main gold
+    300: "#595959", // hover black
+    400: "#FFFFFF", // main white
+    500: "#D1C49D", // hover gold
+  },
+};
+
+const theme = extendTheme({ colors });
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
           <Route
             path="/sell-item"
             element={
@@ -20,7 +33,7 @@ function App() {
                   size="lg"
                   borderRadius="full"
                   boxShadow="md"
-                  _hover={{ boxShadow: 'lg' }}
+                  _hover={{ boxShadow: "lg" }}
                 >
                   Go back to Home
                 </Button>
@@ -30,26 +43,6 @@ function App() {
         </Routes>
       </Router>
     </ChakraProvider>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <p>This is the Home page</p>
-      {/* Button for navigating to Sell Item */}
-      <Link to="/sell-item">
-        <Button
-          colorScheme="teal"
-          size="lg"
-          borderRadius="full"
-          boxShadow="md"
-          _hover={{ boxShadow: 'lg' }}
-        >
-          Go to Sell Item
-        </Button>
-      </Link>
-    </div>
   );
 }
 
