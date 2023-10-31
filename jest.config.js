84d6be524db0b3c0c3deca1ec8599470bdf820e7
mylesshin <myles.h.shin@vanderbilt.ediu>
2023-10-31 10:22:59 -0500

    test all

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    testEnvironment: 'jsdom', // Use the jsdom test environment
-    transform: {
-      "^.+\\.(js|jsx)$": "babel-jest",
-    },
-    moduleFileExtensions: ['js', 'json', 'jsx'],
-  };
-  
+  testEnvironment: 'jsdom', // Use the jsdom test environment
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  moduleFileExtensions: ['js', 'json', 'jsx'],
+  collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**', '!**/coverage/**', '!**/jest.config.js'],
+};
