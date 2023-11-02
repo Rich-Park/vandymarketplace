580086c841e00344345d995df988111d8febe70a
mylesshin <myles.h.shin@vanderbilt.ediu>
2023-11-02 11:27:45 -0500

    accidentally deleted it so just readding

---
+++ src/components/MyItems.js
@@ -0,0 +1,22 @@
+import Header from "./Header";
+import ImageGallery from "./ImageGallery";
+import SearchBar from "./searchbar";
+import { useState } from "react";
+
+export default function MyItems() {
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = (query) => {
+      setSearchQuery(query);
+    };
+
+    return (
+        <div>
+            <Header />
+            <SearchBar onSearch={handleSearch} />
+            <ImageGallery searchQuery={searchQuery} myItems={true}/>
+        </div>
+
+    );
+  }
