d0763bd4fc636f32489f28cac49f07b4fdfa814d
scottkang123 <48114611+scottkang123@users.noreply.github.com>
2023-11-10 23:57:56 -0600

    tests added

---
+++ src/tests/firebaseWrite.test.js
@@ -0,0 +1,115 @@
+import {
+    doc,
+    collection,
+    addDoc,
+    runTransaction,
+    increment,
+    arrayUnion,
+    arrayRemove,
+  } from "firebase/firestore";
+  import {
+    ref,
+    uploadBytes,
+    getDownloadURL,
+  } from "firebase/storage";
+  import { storeItemsSell, likeItem, unlikeItem } from "../firebaseFunctions/firebaseWrite";
+  import { db, storage } from "../firebaseConfig";
+  
+  // Mock Firebase functions
+  jest.mock("firebase/firestore");
+  jest.mock("firebase/storage");
+  
+  describe("Firebase Functions", () => {
+    const userId = "testUserId";
+    const sellerId = "testSellerId";
+    const itemId = "testItemId";
+    const formItem = {
+      productName: "Test Product",
+      email: "test@example.com",
+      price: "50",
+      description: "Test description",
+      images: [{ name: "image1.jpg" }, { name: "image2.jpg" }],
+      tags: ["tag1", "tag2"],
+      timestamp: 123456789,
+    };
+  
+    beforeEach(() => {
+      // Clear all mocks before each test
+      jest.clearAllMocks();
+    });
+  
+    describe("storeItemsSell", () => {
+      it("should store items for sale", async () => {
+        
+        // Mock Storage
+        uploadBytes.mockResolvedValue({});
+        getDownloadURL.mockResolvedValue("mockImageUrl");
+  
+        await storeItemsSell(userId, formItem);
+  
+        // Assertions
+        expect(doc).toHaveBeenCalledWith(db, "users", userId);
+        expect(collection).toHaveBeenCalledWith(doc(), "ItemsToSell");
+        expect(uploadBytes).toHaveBeenCalledTimes(2); // Two images in form_item.images
+        expect(addDoc).toHaveBeenCalledWith(
+            collection(doc(db, "users", userId), "ItemsToSell"),  // Correct collection path
+            {
+              productName: formItem.productName,
+              email: formItem.email,
+              price: parseFloat(formItem.price),
+              description: formItem.description,
+              imageURLs: ["mockImageUrl", "mockImageUrl"], // Two images uploaded
+              tags: formItem.tags,
+              timestamp: formItem.timestamp,
+              likesCount: 0,
+              sellerId: userId,
+            }
+          );    
+
+      });
+    });
+  
+    describe("likeItem", () => {
+      it("should like an item", async () => {
+        // Mock functions
+        const runTransactionMock = jest.fn();
+  
+        // Mock Firestore
+        doc.mockReturnValue({
+          get: jest.fn(() => Promise.resolve({ exists: () => true })),
+        });
+        runTransaction.mockImplementation(runTransactionMock);
+  
+        await likeItem(userId, sellerId, itemId);
+  
+        // Assertions
+        expect(doc).toHaveBeenCalledWith(db, "users", userId);
+        expect(doc).toHaveBeenCalledWith(db, "users", sellerId);
+        expect(doc).toHaveBeenCalledWith(doc(), "ItemsToSell", itemId);
+        expect(runTransactionMock).toHaveBeenCalled();
+        // Additional assertions for the content of the transaction callback are recommended
+      });
+    });
+  
+    describe("unlikeItem", () => {
+      it("should unlike an item", async () => {
+        // Mock functions
+        const runTransactionMock = jest.fn();
+  
+        // Mock Firestore
+        doc.mockReturnValue({
+          get: jest.fn(() => Promise.resolve({ exists: () => true })),
+        });
+        runTransaction.mockImplementation(runTransactionMock);
+  
+        await unlikeItem(userId, sellerId, itemId);
+  
+        // Assertions
+        expect(doc).toHaveBeenCalledWith(db, "users", userId);
+        expect(doc).toHaveBeenCalledWith(db, "users", sellerId);
+        expect(doc).toHaveBeenCalledWith(doc(), "ItemsToSell", itemId);
+        expect(runTransactionMock).toHaveBeenCalled();
+        // Additional assertions for the content of the transaction callback are recommended
+      });
+    });
+  });
